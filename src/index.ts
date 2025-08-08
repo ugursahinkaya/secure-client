0383fc7cf435f73a070813bf46a9762459fa286f
Uğur Şahinkaya <ugur@sahinkaya.xyz>
2025-08-08 14:39:24 +0300

    feat: migrate to monorepo structure
    Restructure project to use monorepo architecture for better code organization,
    shared dependencies management, and streamlined development workflow.

--- src/index.ts
+++ src/index.ts
@@ -9,36 +9,59 @@ import { Logger } from "@ugursahinkaya/logger";
 export class SecureAuth<
   TOperations extends SecureFetchApiOperations,
 > extends GenericRouter<TOperations> {
-  protected api: SecureFetch<SecureFetchApiOperations>;
+  protected api?: SecureFetch<SecureFetchApiOperations>;
   protected authLogger: Logger;
-  constructor(
-    public authUrl: string,
-    operations: TOperations,
-    logLevel?: LogLevel
-  ) {
-    super(operations, logLevel);
-    this.authLogger = new Logger("secure-auth", logLevel);
+  public authUrl: string;
+  public fetchApiInit: () => void;
+  constructor(args: {
+    authUrl: string;
+    operations: TOperations;
+    autoInit?: boolean;
+    logLevel?: LogLevel;
+  }) {
+    super(args.operations, args.logLevel);
+    this.authUrl = args.authUrl;
+    this.authLogger = new Logger("secure-auth", args.logLevel);
     const fetchApiOperations = {
-      ...operations,
+      ...args.operations,
       readyToFetch: () => {
         this.authLogger.debug("readyToFetch", "fetchApiOperations");
 
         this.call("loginOrRegister");
       },
     } as SecureFetchApiOperations;
-
-    this.api = new SecureFetch(authUrl, fetchApiOperations, logLevel);
+    this.fetchApiInit = () => {
+      this.api = new SecureFetch(
+        args.authUrl,
+        fetchApiOperations,
+        args.logLevel
+      );
+    };
+    if (args.autoInit !== false) {
+      this.fetchApiInit();
+    }
   }
   async refresh(token: string) {
     this.authLogger.debug(token, "refresh");
-
-    return this.api.refresh(token);
+    return this.api
+      ? this.api.refresh(token)
+      : this.authLogger.error(
+        "You must call secureAuth.fetchApiInit() when autoInit=false"
+      );
   }
   queryTokenValue() {
-    return this.api.queryTokenValue();
+    return this.api
+      ? this.api.queryTokenValue()
+      : this.authLogger.error(
+        "You must call secureAuth.fetchApiInit() when autoInit=false"
+      );
   }
   async resetPassword(userName: string) {
-    return this.api.fetch(`${this.authUrl}/resetPassword`, { userName });
+    return this.api
+      ? this.api.fetch(`${this.authUrl}/resetPassword`, { userName })
+      : this.authLogger.error(
+        "You must call secureAuth.fetchApiInit() when autoInit=false"
+      );
   }
   async register(
     userName: string,
@@ -48,46 +71,66 @@ export class SecureAuth<
     password2: string
   ) {
     this.authLogger.debug(userName, "register");
-    return this.api.fetch(`${this.authUrl}/register`, {
-      userName,
-      firstName,
-      lastName,
-      password,
-      password2,
-    });
+    return this.api
+      ? this.api.fetch(`${this.authUrl}/register`, {
+        userName,
+        firstName,
+        lastName,
+        password,
+        password2,
+      })
+      : this.authLogger.error(
+        "You must call secureAuth.fetchApiInit() when autoInit=false"
+      );
   }
   async changePassword(userName: string, password: string, password2: string) {
     this.authLogger.debug(userName, "changePassword");
 
-    return this.api.fetch(`${this.authUrl}/changePassword`, {
-      userName,
-      password,
-      password2,
-    });
+    return this.api
+      ? this.api.fetch(`${this.authUrl}/changePassword`, {
+        userName,
+        password,
+        password2,
+      })
+      : this.authLogger.error(
+        "You must call secureAuth.fetchApiInit() when autoInit=false"
+      );
   }
   async validate(userName: string, smsToken: string, validationToken: string) {
     this.authLogger.debug(userName, "validate");
 
-    return this.api.fetch(`${this.authUrl}/validate`, {
-      userName,
-      smsToken,
-      validationToken,
-    });
+    return this.api
+      ? this.api.fetch(`${this.authUrl}/validate`, {
+        userName,
+        smsToken,
+        validationToken,
+      })
+      : this.authLogger.error(
+        "You must call secureAuth.fetchApiInit() when autoInit=false"
+      );
   }
-  async whoIs(queryToken: string) {
+  whoIs = async (queryToken: string) => {
     this.authLogger.debug(queryToken, "whoIs");
 
-    return await this.api.fetch(`${this.authUrl}/getUserData`, {
-      userQueryToken: queryToken,
-    });
-  }
+    return this.api
+      ? await this.api.fetch(`${this.authUrl}/getUserData`, {
+        userQueryToken: queryToken,
+      })
+      : this.authLogger.error(
+        "You must call secureAuth.fetchApiInit() when autoInit=false"
+      );
+  };
   async checkUserName(userName: string, register: boolean) {
     this.authLogger.debug(userName, `checkUserName register:${register}`);
 
-    const res = await this.api.fetch(`${this.authUrl}/checkUserName`, {
-      userName,
-      register,
-    });
+    const res = this.api
+      ? await this.api.fetch(`${this.authUrl}/checkUserName`, {
+        userName,
+        register,
+      })
+      : this.authLogger.error(
+        "You must call secureAuth.fetchApiInit() when autoInit=false"
+      );
     if (res.data === "invalid_user") {
       delete res.data;
       res.error = "Hatalı kullanıcı adı";
@@ -98,17 +141,25 @@ export class SecureAuth<
   async logout() {
     this.authLogger.debug("", `logout`);
 
-    await this.api.fetch(`${this.authUrl}/logout`, {});
+    this.api
+      ? await this.api.fetch(`${this.authUrl}/logout`, {})
+      : this.authLogger.error(
+        "You must call secureAuth.fetchApiInit() when autoInit=false"
+      );
     await this.call("loggedOut", { channel: "rest", secure: true });
     this.authLogger.debug("logged out", `logout`);
   }
   async login(userName: string, password: string) {
     this.authLogger.debug(userName, `login`);
 
-    const loginRes = await this.api.fetch(`${this.authUrl}/login`, {
-      userName,
-      password,
-    });
+    const loginRes = this.api
+      ? await this.api.fetch(`${this.authUrl}/login`, {
+        userName,
+        password,
+      })
+      : this.authLogger.error(
+        "You must call secureAuth.fetchApiInit() when autoInit=false"
+      );
     this.authLogger.debug(loginRes, [`login`, "reslut"]);
 
     if (loginRes.error) {
